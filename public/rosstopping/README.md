677efeac926eaa2c9635395dd3ba4d3588a47727
mobile-michel <info@maillard.li>
2022-04-04 08:27:33 +0200

    Intermediate update

---
+++ public/rosstopping/README.md
@@ -0,0 +1,24 @@
+# Tailwind CSS Layouts & Templates
+
+This repo is a resource of layouts and templates that can be used in any project.
+
+## Demos
+
+You can preview all of the templates and layouts on this landing page here. [Preview Templates](https://templates.digizu.co.uk/)
+
+## High Quality Premium Templates - Novolio
+
+A collection of premium high quality website design templates, built using the latest technologies and ready to download and start using today. 
+ [View Templates](https://novolio.dev)
+
+## Built With
+
+* [Tailwind CSS](https://tailwindcss.com/) - CSS Framework
+
+## Authors
+
+* **Ross Topping** - [Digizu](https://digizu.co.uk)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ src/config.ts
@@ -0,0 +1,40 @@
+export const SITE = {
+    title: "Tailwind templates & components",
+    description: "An ensemble of well designed templates starting from a project of Ross Topping.",
+    footer: '© 2022 Michel Maillard Copyright | info@maillard.li'
+}
+
+export const NAV_ITEMS = {
+    overview: {
+        path: '/',
+        title: 'Home'
+    },
+    templates: {
+        path: './rosstopping/index.html',
+        title: 'Raw templates'
+    },
+    skycrapper: {
+        path: '/skycrapper',
+        title: 'Skycrapper'
+    },
+    fullscreen: {
+        path: '/fullscreen',
+        title: 'Fullscreen'
+    },
+    blocks: {
+        path: '/blocks',
+        title: 'Blocks'
+    },
+    marketing: {
+        path: '/marketing',
+        title: 'Marketing'
+    },
+    product: {
+        path: '/product',
+        title: 'Product'
+    },
+    card: {
+        path: '/card',
+        title: 'Cards'
+    }
+}
